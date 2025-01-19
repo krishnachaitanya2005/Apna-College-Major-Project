b69de421d502bdd06401f6ce5d76dbacc075a58e
krishnachaitanya2005 <chaitanya.jgs@gmail.com>
2025-01-19 12:50:16 +0530

    Initial commit

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initdata = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+	.then(() => {
+		console.log("connected to db");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+async function main() {
+	await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+	await Listing.deleteMany({});
+	initdata.data = initdata.data.map((obj) => ({
+		...obj,
+		owner: "6788e154c7f325a938d4304a",
+	}));
+	await Listing.insertMany(initdata.data);
+	console.log("data initialised");
+};
+
+initDB();
